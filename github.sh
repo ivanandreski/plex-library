0eb12295fe389e21bf4ac4cd65ebcb8a7e290a88
ivan.andreski <ivan.andreski@gmail.com>
2024-03-19 18:44:06 +0100

    chron-scan-2024-03-19_18-44-06

---
+++ github.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Navigate to the root of the repository
+cd "$(git rev-parse --show-toplevel)" || exit
+
+# Add all changes
+git add .
+
+# Commit changes with a message including current timestamp
+commit_message="chron-scan-$(date +"%Y-%m-%d_%H-%M-%S")"
+git commit -m "$commit_message"
+
+# Push changes using the stored access token
+git push https://github.com/${GITHUB_REPOSITORY}.git HEAD:main --force
